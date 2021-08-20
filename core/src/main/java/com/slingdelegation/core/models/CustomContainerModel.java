c721125e176fc6d4ba2fa0166477ec1275a74221
Chris Shaw <me@cshaw.xyz>
2021-08-21 01:39:15 +1000

    Resolve support for AEM SPA Editor

--- core/src/main/java/com/slingdelegation/core/models/CustomContainerModel.java
+++ core/src/main/java/com/slingdelegation/core/models/CustomContainerModel.java
@@ -5,24 +5,31 @@ import com.adobe.cq.export.json.ExporterConstants;
 import com.adobe.cq.wcm.core.components.models.LayoutContainer;
 import com.adobe.cq.wcm.core.components.models.ListItem;
 import com.adobe.cq.wcm.core.components.models.datalayer.ComponentData;
-import com.day.cq.wcm.foundation.model.export.AllowedComponentsExporter;
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.components.ComponentContext;
 import com.day.cq.wcm.foundation.model.responsivegrid.ResponsiveGrid;
-import com.day.cq.wcm.foundation.model.responsivegrid.export.ResponsiveGridExporter;
 import com.drew.lang.annotations.NotNull;
 import com.drew.lang.annotations.Nullable;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.slingdelegation.core.export.CustomContainerExporter;
+import com.slingdelegation.core.support.HierarchyComponentContextWrapper;
 import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.wrappers.SlingHttpServletRequestWrapper;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.Via;
 import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.apache.sling.models.annotations.via.ResourceSuperType;
+import org.apache.sling.models.factory.ModelFactory;
 import org.osgi.annotation.versioning.ProviderType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 import java.util.List;
 import java.util.Map;
 
@@ -41,11 +48,53 @@ import java.util.Map;
 )
 @ProviderType
 public class CustomContainerModel extends ResponsiveGrid implements LayoutContainer, CustomContainerExporter {
-    public static final String RESOURCE_TYPE = "demo/components/container";
+    private static final Logger logger = LoggerFactory.getLogger(CustomContainerModel.class);
+
+    protected static final String RESOURCE_TYPE = "demo/components/container";
+
+    /**
+     * Name of the request attribute that defines whether the page is an entry point of the request.
+     */
+    public static final String ATTR_HIERARCHY_ENTRY_POINT_PAGE = "com.adobe.aem.spa.project.core.models.Page.entryPointPage";
+
+    /**
+     * Request attribute key of the component context
+     */
+    public static final String ATTR_COMPONENT_CONTEXT = "com.day.cq.wcm.componentcontext";
+
+    /**
+     * Request attribute key of the current page
+     */
+    public static final String ATTR_CURRENT_PAGE = "currentPage";
+
+    @Inject
+    private ModelFactory modelFactory;
+
+    @Self
+    private SlingHttpServletRequest request;
 
     @Self
     @Via(type = ResourceSuperType.class)
     private LayoutContainer layoutContainer;
+
+    @PostConstruct
+    protected void postInit() {
+        try {
+            Resource resource = request.getResource();
+            Page currentPage = resource.adaptTo(Page.class);
+
+            // If the 'LayoutContainer' instance is either 'null' or it has no items available then we can
+            // attempt to get a new instance using a wrapped request and adapting it to 'LayoutContainer'.
+            if (layoutContainer == null || layoutContainer.getItems().size() == 0) {
+                layoutContainer = modelFactory.getModelFromWrappedRequest(
+                        createHierarchyServletRequest(request, currentPage, currentPage),
+                        resource,
+                        LayoutContainer.class);
+            }
+        } catch (Exception e) {
+            logger.info("Unable to bind the current page request to 'LayoutContainer'", e);
+        }
+    }
 
     public String getCustomProperty() {
         return "foo";
@@ -100,5 +149,21 @@ public class CustomContainerModel extends ResponsiveGrid implements LayoutContai
     @Override
     public String[] getExportedItemsOrder() {
         return layoutContainer.getExportedItemsOrder();
+    }
+
+    private SlingHttpServletRequest createHierarchyServletRequest(
+            @NotNull SlingHttpServletRequest request,
+            @NotNull com.day.cq.wcm.api.Page page, @Nullable com.day.cq.wcm.api.Page entryPage) {
+        SlingHttpServletRequest wrapperRequest = new SlingHttpServletRequestWrapper(request);
+
+        ComponentContext componentContext = (ComponentContext) request.getAttribute(ATTR_COMPONENT_CONTEXT);
+
+        // When traversing child pages, the currentPage must be updated
+        HierarchyComponentContextWrapper componentContextWrapper = new HierarchyComponentContextWrapper(componentContext, page);
+        wrapperRequest.setAttribute(ATTR_COMPONENT_CONTEXT, componentContextWrapper);
+        wrapperRequest.setAttribute(ATTR_CURRENT_PAGE, page);
+        wrapperRequest.setAttribute(ATTR_HIERARCHY_ENTRY_POINT_PAGE, entryPage);
+
+        return wrapperRequest;
     }
 }
