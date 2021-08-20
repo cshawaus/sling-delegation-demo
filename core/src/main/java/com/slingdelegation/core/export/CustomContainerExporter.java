7c594729a5cb9c3b525906c680869e636bec8cb1
Chris Shaw <me@cshaw.xyz>
2021-08-20 18:11:06 +1000

    Added example for additional property exports

--- core/src/main/java/com/slingdelegation/core/export/CustomContainerExporter.java
+++ core/src/main/java/com/slingdelegation/core/export/CustomContainerExporter.java
@@ -1,7 +1,9 @@
 package com.slingdelegation.core.export;
 
 import com.adobe.cq.export.json.ContainerExporter;
+import com.adobe.cq.wcm.core.components.models.LayoutContainer;
 import com.day.cq.wcm.foundation.model.responsivegrid.export.ResponsiveGridExporter;
+import com.drew.lang.annotations.NotNull;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.osgi.annotation.versioning.ProviderType;
@@ -11,4 +13,8 @@ import org.osgi.annotation.versioning.ProviderType;
 public interface CustomContainerExporter extends ResponsiveGridExporter, ContainerExporter {
     @JsonProperty("myCustomProperty")
     String getCustomProperty();
+
+    @NotNull
+    @JsonProperty("layout")
+    LayoutContainer.LayoutType getLayout();
 }
